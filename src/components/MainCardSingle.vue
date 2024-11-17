b6db271643152692b6afb50a74f7d97e8e0b6ef8
wolfgang595 <147774151+wolfgang595@users.noreply.github.com>
2024-11-17 16:29:20 -0600

    Initial commit

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+import { faker } from '@faker-js/faker'
+
+
+
+const firstname = faker.person.firstName()
+
+const lastname = faker.person.lastName()
+const fullname = faker.person.fullName({ irstName: firstname, lastName: lastname })
+const job_title = faker.person.jobTitle()
+const bio = faker.lorem.lines(2)
+const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
+
+
+</script>
+
+
+<!--  -->
+
+<template>
+    <RouterLink :to="{name: 'CardDetails', params: {id: username} }">
+    <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
+        <img class="object-fill h-480 w-960" v-bind:src="faker.image.urlLoremFlickr({category: 'cats', height: 480, width: 960})" />
+        <div class="p-4">
+            <p class="mb-2 text-2xl font-bold">{{ firstname }} {{ lastname }}</p>
+            <p class="mb-3 font-normal text-gray-800">{{ jobtitle }}</p>
+            <p class="mb-3 font-normal text-gray-800">{{ bio }}</p>
+        </div>
+    </div>
+
+
+    </RouterLink>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+import { useAuth } from '../composables/useAuth';
+
+const { login, logout } = useAuth()
+
+const router = useRouter()
+const route = useRoute()
+
+const username = ref('')
+const password = ref('')
+
+const LogUserIn = async () => {
+
+    if (await login(username.value, password.value)) {
+        if (route.query.redirect) {
+        router.push(route.query.redirect)
+        } else {
+            router.push({name: 'SettingsPage'})
+    } 
+} else {
+        logout()
+    }
+}
+
+</script>
+
+<template>
+    <form class="m-10" @submit.prevent>
+
+        <div class="pb-1">
+            <label for="username" class="block mb-2 font-bold">Username</label>
+            <input id="username" v-model="username" type="text" placeholder ="Username">
+        </div>
+
+        
+        <div>
+            <label for="password" class="block mb-2 font-bold">Password</label>
+            <input id="password" v-model="password" type="password" placeholder="Password">
+        </div>
+
+        <button @click="LogUserIn" class="bg-green-500 mt-4 px-4 py-2 hover:bg-green-800 hover:text-white">Sign In</button>
+
+    </form>
+</template>
