5403fd534bb2c8340c6ab214ebc35eb79476690b
Nathan Serna <nath2534@outlook.com>
2024-11-17 17:58:08 -0600

    modifyed vite config and router

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/homework5/',
+  base: '/homework11/',
   plugins: [vue()],
   resolve: {
     alias: {
